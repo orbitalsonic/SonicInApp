847619abe57f71ce363a289ea68fa646cf7fac02
Sohaib <sohaibahmed.hypersoft@gmail.com>
2022-12-19 11:03:13 +0500

    SonicInAppPurchase

--- app/src/main/java/com/orbitalsonic/sonicinapppurchase/MainActivity.kt
+++ app/src/main/java/com/orbitalsonic/sonicinapppurchase/MainActivity.kt
@@ -2,10 +2,41 @@ package com.orbitalsonic.sonicinapppurchase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.orbitalsonic.sonicinapp.BillingManager
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val billingManager by lazy { BillingManager(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initBilling()
     }
+
+    private fun initBilling() {
+        /*if (BuildConfig.DEBUG) {
+            billingManager.startConnection(billingManager.getDebugProductIDList()) { connectionResult, message ->
+               // binding.mbMakePurchase.isEnabled = connectionResult
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            billingManager.startConnection(listOf(packageName)) { connectionResult, message ->
+               // binding.mbMakePurchase.isEnabled = connectionResult
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            }
+        }*/
+    }
+
+
+    private fun onPurchaseClick() {
+        billingManager.makePurchase { isSuccess, message ->
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+           //binding.mbMakePurchase.isEnabled = !isSuccess
+        }
+    }
+
 }
--- sonicinapp/src/main/java/com/orbitalsonic/sonicinapp/Dummy.kt
+++
@@ -1,4 +0,0 @@
-package com.orbitalsonic.sonicinapp
-
-class Dummy {
-}
